13c3891a716d250a9c1b783460c75950c422f0e2
sekebaev <104671396+sekebaev@users.noreply.github.com>
2022-12-04 14:44:18 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.bars').click(function(event) {
+        $('.bars-menu').toggleClass('active');
+        $('body').toggleClass('overflow');
+          
+    });
+});
+
+$(document).ready(function() {
+    $('.button-close-bars').click(function(event) {
+        $('.bars-menu').removeClass('active')
+        $('body').removeClass('overflow');
+    });
+});
